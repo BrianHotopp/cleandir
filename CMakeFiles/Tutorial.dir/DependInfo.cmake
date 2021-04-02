25997db73f576e6d7aa0534f02ee7218b88f395e
brianhotopp <brihoto@gmail.com>
2021-04-01 22:36:15 -0400

    initial commit

---
+++ CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hotopb/Dropbox/Spring 2021/Open Source Software/labs/dump/lab07/help/cmake-tests/tutorial.cxx" "/home/hotopb/Dropbox/Spring 2021/Open Source Software/labs/dump/lab07/help/cmake-tests/CMakeFiles/Tutorial.dir/tutorial.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "MathFunctions"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/hotopb/Dropbox/Spring 2021/Open Source Software/labs/dump/lab07/help/cmake-tests/MathFunctions/CMakeFiles/MathFunctions.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ README.md
@@ -0,0 +1,2 @@
+# cmake-tests
+source for this is [CMake](https://gitlab.kitware.com/cmake/cmake)
